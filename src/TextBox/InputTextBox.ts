360a40a8fc9cc424f81e5599becd3f095994b1b3
Sidney Liu <cloxnu@outlook.com>
2021-11-25 00:54:11 +0800

    Optimize getter method

--- src/TextBox/InputTextBox.ts
+++ src/TextBox/InputTextBox.ts
@@ -3,14 +3,12 @@ import { UserConfig } from "../Config/Config";
 import TextBox from "./TextBox";
 
 class InputTextBox extends TextBox {
-    protected content?: Component | undefined
-
     constructor(config: UserConfig) {
         super(config);
     }
     
-    get getContent(): Component | undefined {
-        return this.content;
+    get content(): Component | undefined {
+        return undefined;
     }
 }
 
