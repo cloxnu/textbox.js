5b848ee1f6ce411b1dda7339062c6c4c7fe5c172
Sidney Liu <cloxnu@outlook.com>
2021-12-06 01:31:24 +0800

    Change `test.ts` to `test.js` , more conveniently call `page.evalute()` (#32)

---
+++ src/Tests/UI/TextBox/normal.test.js
@@ -0,0 +1,50 @@
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
+const sleep = require('../../test_utils');
+
+let page;
+const url = `file://${__dirname}/../test.html`;
+
+// https://github.com/americanexpress/jest-image-snapshot
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+    failureThreshold: 100,
+    failureThresholdType: 'pixel',
+});
+expect.extend({ toMatchImageSnapshot });
+
+beforeEach(async () => {
+    page = await global.__BROWSER__.newPage();
+    return page.goto(url);
+});
+
+afterEach(async () => {
+    return page.close();
+});
+
+describe('TextBox normal', () => {
+    test('render', async () => {
+        await page.evaluate(() => {
+            textbox();
+        });
+        await sleep(800);
+        expect(await page.screenshot()).toMatchImageSnapshot();
+    });
+
+    test('hide', async () => {
+        let actual = await page.evaluate(() => {
+            function get_opacity() {
+                return getComputedStyle(tb.element).getPropertyValue('opacity');
+            }
+            let tb = textbox();
+            // const show_opacity = get_opacity();
+            tb.hide();
+            const hide_opacity = get_opacity();
+            return {
+                // show_opacity: show_opacity,
+                hide_opacity: hide_opacity,
+            }
+        });
+
+        // expect(actual.show_opacity).toBe('1');
+        expect(actual.hide_opacity).toBe('0');
+    });
+});
