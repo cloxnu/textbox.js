b58183d65a69aa9c94a880366d9c3438faa6b974
Sidney Liu <cloxnu@outlook.com>
2022-01-01 16:09:25 +0800

    opt `ComponentConfig` (#47)

--- src/Config/ComponentConfig.ts
+++ src/Config/ComponentConfig.ts
@@ -1,6 +1,13 @@
+import utils from "../utils";
 import { ConfigManager, InnerConfig } from "./Config";
 
 type CssItemType = {property: string, value: string, priority?: string};
+enum Type {
+    Number,
+    String,
+    Array,
+    Dictionary,
+}
 
 class ComponentConfig extends InnerConfig {
     id?: string;
@@ -9,36 +16,65 @@ class ComponentConfig extends InnerConfig {
     color?: string;
     bgColor?: string;
     
-    css?: CssItemType[];
+    css?: CssItemType[] | CssItemType;
+    csskv?: object;
 
     setIn(element: HTMLElement) {
-        check(this.id, obj => element.id = obj);
-        check(this.class, obj => {
-            switch (typeof obj) {
-                case 'string':
-                    element.classList.add(obj);
-                    break;
-                case 'object':
-                    if (Array.isArray(obj)) {
-                        element.classList.add(...obj);
-                    }
-                    break;
+        check(this.id, Type.String, obj => element.id = obj);
+
+        check(this.class, Type.String, obj => {
+            element.classList.add(obj);
+        });
+        check(this.class, Type.Array, obj => {
+            element.classList.add(...obj);
+        });
+
+        check(this.innerHTML, Type.String, obj => element.innerHTML = obj);
+        check(this.color, Type.String, obj => element.style.color = obj);
+        check(this.bgColor, Type.String, obj => element.style.backgroundColor = obj);
+
+        check(this.css, Type.Dictionary, obj => {
+            if ('property' in obj && 'value' in obj) {
+                element.style.setProperty(obj.property, obj.value, obj.priority);
             }
         });
-        check(this.innerHTML, obj => element.innerHTML = obj);
-        check(this.color, obj => element.style.color = obj);
-        check(this.bgColor, obj => element.style.backgroundColor = obj);
-
-        check(this.css, obj => {
+        check(this.css, Type.Array, obj => {
             obj.forEach((item: CssItemType) => {
-                element.style.setProperty(item.property, item.value, item.priority);
+                if ('property' in item && 'value' in item) {
+                    element.style.setProperty(item.property, item.value, item.priority);
+                }
+            });
+        });
+
+        check(this.csskv, Type.Dictionary, obj => {
+            for (const key in obj) {
+                if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                    const value = obj[key];
+                    element.style.setProperty(key, value);
+                }
+            }
+        });
+        check(this.csskv, Type.Array, obj => {
+            obj.forEach((item: any) => {
+                if (utils.isObject(item)) {
+                    for (const key in obj) {
+                        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                            const value = obj[key];
+                            element.style.setProperty(key, value);
+                        }
+                    }
+                }
             });
         });
     }
 }
 
-function check(obj: any, block: (obj: any) => void) {
-    if (obj) {
+function check(obj: any, type: Type, block: (obj: any) => void) {
+    if (obj &&
+        (type === Type.Number && typeof(obj) === 'number') ||
+        (type === Type.String && typeof(obj) === 'string') ||
+        (type === Type.Array && utils.isArray(obj)) ||
+        (type === Type.Dictionary && utils.isObject(obj))) {
         block(obj);
     }
 }
